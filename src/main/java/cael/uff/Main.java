d0b4d31523a305d6efbe1c8d9a8507c1e6ed02f3
B-Cut <caelgonsalvesrj@hotmail.com>
2024-11-17 18:35:25 -0300

    Refactored main

--- src/main/java/cael/uff/Main.java
+++ src/main/java/cael/uff/Main.java
@@ -6,11 +6,14 @@ package cael.uff;
         - System Tests
 */
 
+import cael.uff.classification.framework.FrameworkClassifier;
 import cael.uff.classification.heuristics.FilepathClassifier;
 import cael.uff.classification.heuristics.FunctionClassifier;
 import cael.uff.classification.Result;
 import cael.uff.classification.TestPhases;
+import cael.uff.classification.heuristics.HeuristicsClassifier;
 
+import java.awt.*;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -19,12 +22,17 @@ import java.util.List;
 public class Main {
     private final static String testFolderName = "test";
 
+    // TODO: Proper argument parsing
     public static void main(String[] args) {
         if (args.length != 1) {
-            System.out.println("Usage: java -jar heuristics-classificator.jar <path-to-repository>");
+            System.out.println(
+                    "Usage: java -jar tests-classificator.jar <path-to-repository> <keyword-json> <libraries-json>"
+            );
         }
 
         Path repoPath = Paths.get(args[0]);
+        Path keywordPath = Paths.get(args[1]);
+        Path librariesPath = Paths.get(args[2]);
 
         List<Path> testFolders;
 
@@ -35,43 +43,12 @@ public class Main {
             return;
         }
 
-        Result res = new Result();
+        HeuristicsClassifier heuristicsClassifier = new HeuristicsClassifier(keywordPath);
 
-        // There are folders labeled as "test" in repository
-        if(!testFolders.isEmpty()){
-            for(Path testFolder : testFolders){
-                FilepathClassifier classifier = new FilepathClassifier();
-                classifier.classifyFiles(testFolder, res);
-            }
+        testFolders.forEach(heuristicsClassifier::classify);
 
-            // First we classify the tests based on the identified file paths
-
-            // Not proper but works
-            for(Path unitFolder : res.unitFolders){
-                FunctionClassifier.massClassifyFunctionOnDirectory(unitFolder, TestPhases.UNIT, res);
-            }
-            for(Path integrationFolder : res.integrationFolders){
-                FunctionClassifier.massClassifyFunctionOnDirectory(integrationFolder, TestPhases.INTEGRATION, res);
-            }
-            for(Path systemFolders : res.unitFolders){
-                FunctionClassifier.massClassifyFunctionOnDirectory(systemFolders, TestPhases.SYSTEM, res);
-            }
-
-            for(Path unitFiles : res.unitFiles){
-                FunctionClassifier.massClassifyFunctionOnFile(unitFiles, TestPhases.UNIT, res);
-            }
-            for(Path integrationFiles : res.integrationFiles){
-                FunctionClassifier.massClassifyFunctionOnFile(integrationFiles, TestPhases.INTEGRATION, res);
-            }
-            for(Path systemFiles : res.systemFiles){
-                FunctionClassifier.massClassifyFunctionOnFile(systemFiles, TestPhases.SYSTEM, res);
-            }
-
-            // Then, we have to work with the unclassified files
-            for(Path unclassifiedFiles : res.unclassifiedFiles){
-                FunctionClassifier.classifyFunctionsInFile(unclassifiedFiles, res);
-            }
-        }
+        FrameworkClassifier frameworkClassifier = new FrameworkClassifier(librariesPath);
+        testFolders.forEach(frameworkClassifier::classify);
     }
 
     // TODO: Serialize result
